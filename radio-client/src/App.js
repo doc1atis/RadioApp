7349e9451ace43a39e25138e31e9f6c95005aa4b
Olgy Jeangilles <tlogipaisupremology@yahoo.com>
2020-02-06 12:55:05 -0500

    fixed navBar import

--- radio-client/src/App.js
+++ radio-client/src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
-import NavBar from "./components/NavBar/NavBar";
 import PlayerBar from "./components/PlayerBar/PlayerBar";
-import Navbar from './components/Navbar/Navbar';
-import Body from './components/Body/Body';
+import Navbar from "./components/Navbar/Navbar";
+import Body from "./components/Body/Body";
 
 function App() {
   return (
