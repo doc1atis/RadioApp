d6a21a1b4772f61552d2c34b19feacb6663fa5a9
Olgy Jeangilles <tlogipaisupremology@yahoo.com>
2020-02-06 13:50:18 -0500

    api folderadded

--- radio-client/src/App.js
+++ radio-client/src/App.js
@@ -1,12 +1,7 @@
 import React from "react";
 import PlayerBar from "./components/PlayerBar/PlayerBar";
-<<<<<<< HEAD
-import NavBar from "./components/NavBar/NavBar";
+import NavBar from "./components/NavBar/Navbar";
 import Body from "./components/Body/Body";
-=======
-import NavBar from './components/NavBar/Navbar';
-import Body from './components/Body/Body';
->>>>>>> 088320205399a30262b2c52cce034b48647b651d
 
 function App() {
   return (
