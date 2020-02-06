25dcadd802702b2e3bb45532f6f56abb66c5120f
Olgy Jeangilles <tlogipaisupremology@yahoo.com>
2020-02-06 10:15:18 -0500

    User model created

--- radio-client/src/App.js
+++ radio-client/src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import NavBar from "./components/NavBar/NavBar";
 import PlayerBar from "./components/PlayerBar/PlayerBar";
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <PlayerBar />
     </div>
   );
